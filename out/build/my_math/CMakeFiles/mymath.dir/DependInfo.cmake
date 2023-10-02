92c6733c19fa50445b8da789d21a5cfd76ca0a56
laptop-slava <vch7lv@ya.ru>
2023-10-02 18:21:38 +0300

    убрал ошибки с г++

---
+++ out/build/my_math/CMakeFiles/mymath.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/vch7lv/Projects/my_math/my_math/sourses/bigint.cpp" "my_math/CMakeFiles/mymath.dir/sourses/bigint.cpp.o" "gcc" "my_math/CMakeFiles/mymath.dir/sourses/bigint.cpp.o.d"
+  "/home/vch7lv/Projects/my_math/my_math/sourses/main.cpp" "my_math/CMakeFiles/mymath.dir/sourses/main.cpp.o" "gcc" "my_math/CMakeFiles/mymath.dir/sourses/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
